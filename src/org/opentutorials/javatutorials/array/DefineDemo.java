189505ecf13ac6a5972f975015ed7597654b4946
Nekisse <lsh891224@gmail.com>
2017-05-01 22:40:33 +0900

    going java

---
+++ src/org/opentutorials/javatutorials/array/DefineDemo.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.array;
+
+/**
+ * Created by Nekisse_lee on 2017. 4. 30..
+ */
+public class DefineDemo {
+    public static void main(String[] args) {
+        String [] classGrop = {"최진혁", "최유빈", "한이람", "이고잉"};
+        System.out.println(classGrop[0]);
+        System.out.println(classGrop[1]);
+        System.out.println(classGrop[2]);
+        System.out.println(classGrop[3]);
+    }
+}
---
+++ src/org/opentutorials/javatutorials/array/GetDemo.java
@@ -0,0 +1,10 @@
+package org.opentutorials.javatutorials.array;
+
+/**
+ * Created by Nekisse_lee on 2017. 5. 1..
+ */
+public class GetDemo {
+    public static void main(String[] args) {
+        String[] classGroup = {"최진혁"}
+    }
+}
