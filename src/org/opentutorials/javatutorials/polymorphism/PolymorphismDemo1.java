c695f446a83adae93833e9a092d6ea7b33ed9e96
Nekisse <lsh891224@gmail.com>
2017-05-09 15:54:56 +0900

    E going java

---
+++ src/org/opentutorials/javatutorials/polymorphism/PolymorphismDemo1.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials.polymorphism;
+class A{
+    public String x(){return "A.x";}
+}
+class B extends A{
+    public String x(){return "B.x";}
+    public String y(){return "y";}
+}
+class B2 extends A{
+    public String x(){return "B2.x";}
+}
+public class PolymorphismDemo1 {
+    public static void main(String[] args) {
+        A obj = new B();
+        A obj2 = new B2();
+        System.out.println(obj.x());
+        System.out.println(obj2.x());
+    }
+}
