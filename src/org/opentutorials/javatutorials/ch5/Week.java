3c47f2186716f0966df2e20cc83e59f57f68645c
Nekisse <lsh891224@gmail.com>
2017-05-19 19:58:53 +0900

    이것이 자바다

---
+++ src/org/opentutorials/javatutorials/ch5/Week.java
@@ -0,0 +1,14 @@
+package org.opentutorials.javatutorials.ch5;
+
+/**
+ * Created by Nekisse_lee on 2017. 5. 19..
+ */
+public enum Week {
+    MONDAY,
+    TUESDADY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURADAY,
+    SUNDAY
+}
