5e72b6424ec86a81601b43953df523946cef9acc
Nekisse <lsh891224@gmail.com>
2017-06-18 19:59:33 +0900

    이것이 자바다 익명객체 생성 및 사용 복습

---
+++ src/org/opentutorials/javatutorials/ch9/sec05/exam01_anonymous_extends/Anonymous.java
@@ -0,0 +1,38 @@
+package org.opentutorials.javatutorials.ch9.sec05.exam01_anonymous_extends;
+
+/**
+ * Created by Nekisse_lee on 2017. 6. 18..
+ */
+public class Anonymous {
+    Person field = new Person(){
+        String studentNo;
+        void work(){
+            System.out.println("출근합니다.");
+        }
+        @Override
+        void wake(){
+            System.out.println("6시에 일어납니다.");
+            work();
+        };
+    };
+
+    void method1(){
+        Person localVar = new Person(){
+            String studentNo;
+            void walk(){
+                System.out.println("산책  합니다.");
+            }
+            @Override
+            void wake(){
+                System.out.println("7시에 일어납니다.");
+                walk();
+            };
+        };
+
+        localVar.wake();
+    }
+
+    void method2(Person person){
+        person.wake();
+    }
+}
---
+++ src/org/opentutorials/javatutorials/ch9/sec05/exam01_anonymous_extends/Person.java
@@ -0,0 +1,11 @@
+package org.opentutorials.javatutorials.ch9.sec05.exam01_anonymous_extends;
+
+/**
+ * Created by Nekisse_lee on 2017. 6. 18..
+ */
+public class Person {
+    String name;
+    void wake(){
+        System.out.println("7시에 일어납니다.");
+    }
+}
