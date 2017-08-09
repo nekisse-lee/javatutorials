d4e66fe954f21bf61fbaa055f5b505b4b6212677
Nekisse <lsh891224@gmail.com>
2017-08-09 13:10:12 +0900

    복습

--- src/baekjoon/test.java
+++ src/baekjoon/test.java
@@ -1,7 +1,19 @@
 package baekjoon;
 
+import java.util.Scanner;
+
 /**
  * Created by Nekisse_lee on 2017. 8. 5..
  */
 public class test {
+    public static void main(String[] args) {
+        int sum =0;
+        for (int i = 1; i <= 100; i++) {
+            if (i%3==0){
+                System.out.println(i);
+                sum += i;
+                System.out.println();
+            }
+        }
+    }
 }
---
+++ src/org/opentutorials/javatutorials/ch5/verify/Exercese07.java
@@ -0,0 +1,19 @@
+package org.opentutorials.javatutorials.ch5.verify;
+
+/**
+ * Created by Nekisse_lee on 2017. 8. 8..
+ */
+public class Exercese07 {
+    public static void main(String[] args) {
+        int max = 0;
+        int[] array = {1, 5, 3, 8, 2};
+        for(int arr : array){
+            if (arr>max){
+               max= arr;
+            }
+
+        }
+
+        System.out.println("max: "  + max);
+    }
+}
