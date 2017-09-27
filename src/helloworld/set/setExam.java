7ff09f95c96a7e45afdfc3db64d8e224ff53310a
nekisse <lsh891224@gmail.com>
2017-09-27 18:56:33 +0900

    *.class  ignore

--- src/helloworld/set/setExam.java
+++ src/helloworld/set/setExam.java
@@ -11,7 +11,7 @@ public class setExam {
     public static void main(String[] args) {
         Set<String> set1 = new HashSet<>();
         boolean flag1 = set1.add("kang");
-        boolean flag2 = set1.add("kim");
+        boolean flag2 = set1.add("kim!");
         boolean flag3 = set1.add("kang");
 
         System.out.println(set1.size());
